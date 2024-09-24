c4cd4c35b7c4f175f16da31dee349d346e2a2393
LilianaPopa-stud <popliliaana@gmail.com>
2024-09-24 17:38:08 +0300

    Initial Commit

---
+++ HomeAssesment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HomeAssesment
+//
+//  Created by Liliana Popa on 24.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HomeAssesment/HomeAssesmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAssesmentApp.swift
+//  HomeAssesment
+//
+//  Created by Liliana Popa on 24.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct HomeAssesmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
